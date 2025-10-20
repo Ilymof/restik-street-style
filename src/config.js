2a0fcc0cc95b69380d423cf2dc1ec1ed5dc46bdb
Ilymof <jeffrey.ilymof@mail.ru>
2025-10-21 01:03:17 +0300

    1m refresh

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ const JWT = {
    accessSecret: process.env.ACCESS_TOKEN_SECRET,
    refreshSecret: process.env.REFRESH_TOKEN_SECRET,
    accessExpiresIn: '15s',
-   refreshExpiresIn: '30d'
+   refreshExpiresIn: '1m'
 }
 
 module.exports = { JWT }

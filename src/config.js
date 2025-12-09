455c166eea975a6bd74329d70c8de280d07686e5
Ilymof <jeffrey.ilymof@mail.ru>
2025-12-09 16:18:59 +0300

    cfg настройки

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const process = require('node:process')
 const JWT = {
    accessSecret: process.env.ACCESS_TOKEN_SECRET,
    refreshSecret: process.env.REFRESH_TOKEN_SECRET,
-   accessExpiresIn: '15s',
-   refreshExpiresIn: '30s'
+   accessExpiresIn: '15m',
+   refreshExpiresIn: '30d'
 }
 
 module.exports = { JWT }

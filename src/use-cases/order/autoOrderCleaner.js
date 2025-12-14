e9f428b00a740fc41f564a4e327bdc3e8e2d4c86
Ilymof <jeffrey.ilymof@mail.ru>
2025-12-15 00:06:07 +0300

    new time for autoCleaner

--- src/use-cases/order/autoOrderCleaner.js
+++ src/use-cases/order/autoOrderCleaner.js
@@ -6,7 +6,7 @@ const safeDbCall = require('../../lib/safeDbCall');
 const throwValidationError = require('../../lib/ValidationError')
 
 const cleanOrders = async () => {
-    const sql = `DELETE FROM orders WHERE created_at < NOW() - INTERVAL '60 minutes' AND current_status = 'Отменён';`
+    const sql = `DELETE FROM orders WHERE current_status = 'Отменён'AND created_at < (NOW() AT TIME ZONE 'Europe/Moscow') - INTERVAL '60 minutes';`
     try {
     const result = await safeDbCall(() => orders.query(sql, []))
     console.log(`Удалено истёкших заказов: ${result.rowCount}`);

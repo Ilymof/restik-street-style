3610271fb282550925db3ac1404e56989db72adb
Ilymof <jeffrey.ilymof@mail.ru>
2026-01-21 17:49:21 +0300

    little fix

--- src/schemas/orderMetaSchema.js
+++ src/schemas/orderMetaSchema.js
@@ -9,16 +9,16 @@ const CreateOrderSchema = Schema.from({
     cutlery_quantity:'?number', 
     order_comment: '?string' ,
     created_at: '?string',
-    secret_key: 'string'
+    secret_key: { type: 'string', length: { min: 1 } },
 })
-const UpdateOrderSchema = Schema.from({
-    id: 'number',
-    name: '?string',
-    phone: '?string',
-    'delivery?':{status: 'boolean',address: 'string',comment: '?string', delivery_price: '?number'},
-    dishes: {array: {id: 'number',quantity: 'number', size: '?string'}, required: false },
-    cutlery_status: '?boolean',
-    cutlery_quantity:'?number', 
-    order_comment: '?string',
-})
-module.exports = { CreateOrderSchema, UpdateOrderSchema }
+// const UpdateOrderSchema = Schema.from({
+//     id: 'number',
+//     name: '?string',
+//     phone: '?string',
+//     'delivery?':{status: 'boolean',address: 'string',comment: '?string', delivery_price: '?number'},
+//     dishes: {array: {id: 'number',quantity: 'number', size: '?string'}, required: false },
+//     cutlery_status: '?boolean',
+//     cutlery_quantity:'?number', 
+//     order_comment: '?string',
+// })
+module.exports = { CreateOrderSchema}
